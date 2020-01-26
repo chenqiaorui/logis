de8ea393090b7ac28633ad613450003773637538
meowlomoDeveloper <dev.support@meowlomo.com>
2020-01-26 16:00:11 +0800

    first commit

---
+++ src/main/java/com/antbell/spring/LogisApplication.java
@@ -0,0 +1,13 @@
+package com.antbell.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisApplication.class, args);
+	}
+
+}
